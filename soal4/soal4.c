b4ce942d71de8c0d612f7eb6c68efab604e191fb
VYPRATAMA009 <42761818+VYPRATAMA009@users.noreply.github.com>
2019-04-05 21:14:11 +0700

    Update soal4.c

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -1,4 +1,4 @@
-include<stdio.h>
+#include<stdio.h>
 #include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
@@ -6,33 +6,29 @@ include<stdio.h>
 #include<sys/types.h>
 #include<sys/wait.h>
 
-pthread_t treid3,treid1,treid2; //inisialisasi array untuk menampung thread dalam kasus ini ada 2 thread
+pthread_t treid3,treid1,treid2; /
 
 
 void* ambil(){
-        system("ps -aux | head -11 | cat > /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
-        system("ps -aux | head -11 | cat > /home/vinsensius009/Documents/FolderProses2/SimpanProses2.txt");
+        system("ps -aux | head -11  > /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
+        system("ps -aux | head -11  > /home/vinsensius009/Documents/FolderProses2/SimpanProses2.txt");
 
 }
 
 void* zipkan (){
-         system("zip -r /home/vinsensius009/Documents/FolderProses1/KompresProses1.zip /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
+         system("zip -r /home/vinsensius009/Documents/FolderProses1/KompresProses1 /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
          system("rm /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
  
-        system("zip -r /home/vinsensius009/Documents/FolderProses2/KompresProses2.zip /home/vinsensius009/Documents/FolderProses2/SimpanProses2.txt");
-//      system("rm /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
+        system("zip -r /home/vinsensius009/Documents/FolderProses2/KompresProses2 /home/vinsensius009/Documents/FolderProses2/SimpanProses2.txt");
          system("rm /home/vinsensius009/Documents/FolderProses2/SimpanProses2.txt");
 
 }
 
-//void* hapus () {
-//      system("rm /home/vinsensius009/Documents/FolderProses1/SimpanProses1.txt");
-        //system("rm /home/vinsensius009/Documents/FolderProses2/SimpanProses2.txt");
-//}
+
 void* unzipkan1 (){
 
-        system("unzip /home/vinsensius009/Documents/FolderProses1/KompresProses1.zip");
-        system("unzip /home/vinsensius009/Documents/FolderProses2/KompresProses2.zip");
+        system("unzip /home/vinsensius009/Documents/FolderProses1/KompresProses1 -d /home/vinsensius009/Documents/FolderProses1");
+        system("unzip /home/vinsensius009/Documents/FolderProses2/KompresProses2 -d /home/vinsensius009/Documents/FolderProses2" );
 }
 
 
@@ -45,7 +41,7 @@ int main(void)
     pthread_join(treid2,NULL);
     printf("Menunggu 15 detik untuk mengekstrak kembali");
     
-sleep(5);
+//sleep(5);
     printf("tes");
     pthread_create(&(treid3), NULL, unzipkan1, NULL);
     pthread_join(treid3, NULL);
@@ -53,5 +49,6 @@ sleep(5);
 
     return 0;
 }
+
 
 
